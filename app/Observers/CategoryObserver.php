e75fe54f9343fd71e8c9b6b77df1ea0f85f38e30
Zoran Bogoevski <zbogoevski@gmail.com>
2021-11-23 13:53:33 +0100

    update observer

--- app/Observers/CategoryObserver.php
+++ app/Observers/CategoryObserver.php
@@ -1,63 +1,64 @@
 <?php
 
-namespace App\Observers;
+    namespace App\Observers;
 
-use App\Models\Category;
-use Illuminate\Support\Str;
+    use App\Models\Category;
+    use Illuminate\Support\Str;
 
-class CategoryObserver
-{
-    /**
-     * Handle the Category "created" event.
-     *
-     * @param Category $category
-     * @return void
-     */
-    public function creating(Category $category)
+    class CategoryObserver
     {
-    }
+        /**
+         * Handle the Category "created" event.
+         *
+         * @param  Category  $category
+         * @return void
+         */
+        public function creating(Category $category)
+        {
+            $category->slug = Str::slug($category->title);
+        }
 
-    /**
-     * Handle the Category "updated" event.
-     *
-     * @param Category $category
-     * @return void
-     */
-    public function updated(Category $category)
-    {
-        $category->slug = Str::slug($category->title);
-    }
+        /**
+         * Handle the Category "updated" event.
+         *
+         * @param  Category  $category
+         * @return void
+         */
+        public function updated(Category $category)
+        {
+            $category->slug = Str::slug($category->title);
+        }
 
-    /**
-     * Handle the Category "deleted" event.
-     *
-     * @param Category $category
-     * @return void
-     */
-    public function deleted(Category $category)
-    {
-        //
-    }
+        /**
+         * Handle the Category "deleted" event.
+         *
+         * @param  Category  $category
+         * @return void
+         */
+        public function deleted(Category $category)
+        {
+            //
+        }
 
-    /**
-     * Handle the Category "restored" event.
-     *
-     * @param Category $category
-     * @return void
-     */
-    public function restored(Category $category)
-    {
-        //
-    }
+        /**
+         * Handle the Category "restored" event.
+         *
+         * @param  Category  $category
+         * @return void
+         */
+        public function restored(Category $category)
+        {
+            //
+        }
 
-    /**
-     * Handle the Category "force deleted" event.
-     *
-     * @param Category $category
-     * @return void
-     */
-    public function forceDeleted(Category $category)
-    {
-        //
+        /**
+         * Handle the Category "force deleted" event.
+         *
+         * @param  Category  $category
+         * @return void
+         */
+        public function forceDeleted(Category $category)
+        {
+            //
+        }
     }
-}
