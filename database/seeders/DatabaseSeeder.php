c07d70465980ea83e73a94e6f36ec2d21c1d6699
Zoran Bogoevski <zbogoevski@gmail.com>
2021-12-03 15:12:44 +0100

    fix tag bug in blade for post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,36 +1,29 @@
 <?php
 
-    namespace Database\Seeders;
+	namespace Database\Seeders;
 
-    use App\Models\CategoryPost;
-    use App\Models\Gallery;
-    use App\Models\LanguagePost;
-    use App\Models\Post;
-    use App\Models\Tag;
-    use Illuminate\Database\Eloquent\Model;
-    use Illuminate\Database\Seeder;
-    use Illuminate\Support\Facades\DB;
+	use App\Models\Tag;
+	use Illuminate\Database\Eloquent\Model;
+	use Illuminate\Database\Seeder;
 
-    class DatabaseSeeder extends Seeder
-    {
-        /**
-         * Run the database seeders.
-         *
-         * @return void
-         */
-        public function run()
-        {
-            Model::unguard();
-            $this->call(AdminSeeder::class);
+	class DatabaseSeeder extends Seeder {
+		/**
+		 * Run the database seeders.
+		 *
+		 * @return void
+		 */
+		public function run() {
+			Model::unguard();
+			$this->call( AdminSeeder::class );
 //            Model::reguard();
-            $this->call(LanguagesTableSeeder::class);
-            $this->call(CategoriesTableSeeder::class);
-            Tag::factory()->count(100)->create();
-            Post::factory()->count(500)->create();
-            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-            CategoryPost::factory()->count(5000)->create();
-            Gallery::factory()->count(5000)->create();
-            LanguagePost::factory()->count(500)->create();
-            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        }
-    }
+			$this->call( LanguagesTableSeeder::class );
+			$this->call( CategoriesTableSeeder::class );
+			Tag::factory()->count( 100 )->create();
+//            Post::factory()->count(500)->create();
+//            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+//            CategoryPost::factory()->count(5000)->create();
+//            Gallery::factory()->count(5000)->create();
+//            LanguagePost::factory()->count(500)->create();
+//            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+		}
+	}
